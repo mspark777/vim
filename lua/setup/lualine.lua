92eae4f9ff53803eaa2c31d552e8775cac422798
minsoo.park <topangel777@gmail.com>
2022-12-22 10:42:44 +0900

    Update lualine

--- lua/setup/lualine.lua
+++ lua/setup/lualine.lua
@@ -2,4 +2,17 @@ require('lualine').setup({
   options = {
     theme = 'vscode'
   },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {
+      {
+        'filename',
+        path = 1
+      },
+    },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
 })
