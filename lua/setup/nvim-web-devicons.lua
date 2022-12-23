08cd7f22035075011b77b21b583b77a5d45078e2
minsoo.park <topangel777@gmail.com>
2022-12-23 09:36:51 +0900

    added mts

--- lua/setup/nvim-web-devicons.lua
+++ lua/setup/nvim-web-devicons.lua
@@ -1,4 +1,14 @@
-require('nvim-web-devicons').setup({
+local devicons = require('nvim-web-devicons')
+devicons.setup({
   color_icons = true;
   default = true;
+})
+
+devicons.set_icon({
+  ['mts'] = {
+    icon = "",
+    color = "#519aba",
+    cterm_color = "67",
+    name = "Mts",
+  }
 })
