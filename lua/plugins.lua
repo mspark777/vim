4855d9fa013ce6c7cd80853a4b9816672eb35aef
mspark777 <topangel777@gmail.com>
2023-02-07 17:48:34 +0900

    Add: Markdown preview.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,68 +22,74 @@ end
 local packer = require('packer')
 
 return packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'mg979/vim-visual-multi'
-  use 'lukas-reineke/indent-blankline.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'sheerun/vim-polyglot'
-  use 'mfussenegger/nvim-dap'
-  use 'prisma/vim-prisma'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end,
-    config = setup('treesitter')
-  }
-  use {
-    "rest-nvim/rest.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-    config = setup('rest-nvim')
-  }
-  use {
-    'theHamsta/nvim-dap-virtual-text',
-    requires = { 'mfussenegger/nvim-dap', 'nvim-treesitter/nvim-treesitter' },
-    config = setup('nvim-dap-virtual-text')
-  }
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-    requires = { 'nvim-lua/plenary.nvim' }
-  }
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = setup('gitsigns')
-  }
-  use {
-    'neoclide/coc.nvim',
-    branch = 'release'
-  }
-  use {
-    'Mofiqul/vscode.nvim',
-    config = setup('vscode')
-  }
-  use {
-    'kyazdani42/nvim-web-devicons',
-    config = setup('nvim-web-devicons')
-  }
-  use {
-    'romgrk/barbar.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' }
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = setup('lualine')
-  }
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = setup('nvim-tree')
-  }
+      use 'wbthomason/packer.nvim'
+      use 'mg979/vim-visual-multi'
+      use 'lukas-reineke/indent-blankline.nvim'
+      use 'nvim-lua/plenary.nvim'
+      use 'sheerun/vim-polyglot'
+      use 'mfussenegger/nvim-dap'
+      use 'prisma/vim-prisma'
+      use({
+          "iamcco/markdown-preview.nvim",
+          run = "cd app && npm install",
+          setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+          ft = { "markdown" },
+      })
+      use {
+          'nvim-treesitter/nvim-treesitter',
+          run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+          end,
+          config = setup('treesitter')
+      }
+      use {
+          "rest-nvim/rest.nvim",
+          requires = { "nvim-lua/plenary.nvim" },
+          config = setup('rest-nvim')
+      }
+      use {
+          'theHamsta/nvim-dap-virtual-text',
+          requires = { 'mfussenegger/nvim-dap', 'nvim-treesitter/nvim-treesitter' },
+          config = setup('nvim-dap-virtual-text')
+      }
+      use {
+          'nvim-telescope/telescope.nvim',
+          tag = '0.1.1',
+          requires = { 'nvim-lua/plenary.nvim' }
+      }
+      use {
+          'lewis6991/gitsigns.nvim',
+          config = setup('gitsigns')
+      }
+      use {
+          'neoclide/coc.nvim',
+          branch = 'release'
+      }
+      use {
+          'Mofiqul/vscode.nvim',
+          config = setup('vscode')
+      }
+      use {
+          'kyazdani42/nvim-web-devicons',
+          config = setup('nvim-web-devicons')
+      }
+      use {
+          'romgrk/barbar.nvim',
+          requires = { 'kyazdani42/nvim-web-devicons' }
+      }
+      use {
+          'nvim-lualine/lualine.nvim',
+          requires = { 'kyazdani42/nvim-web-devicons' },
+          config = setup('lualine')
+      }
+      use {
+          'kyazdani42/nvim-tree.lua',
+          requires = { 'kyazdani42/nvim-web-devicons' },
+          config = setup('nvim-tree')
+      }
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+      if packer_bootstrap then
+        require('packer').sync()
+      end
+    end)
