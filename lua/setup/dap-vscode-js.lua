eb28bd80256c7c72ad29ad878550417aa8402d39
mspark777 <topangel777@gmail.com>
2023-04-25 10:16:18 +0900

    Update scripts.

--- lua/setup/dap-vscode-js.lua
+++ lua/setup/dap-vscode-js.lua
@@ -1,3 +1,3 @@
 require("dap-vscode-js").setup({
-    adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+    adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
 })
--- lua/setup/nvim-web-devicons.lua
+++ lua/setup/nvim-web-devicons.lua
@@ -1,14 +1,5 @@
 local devicons = require('nvim-web-devicons')
 devicons.setup {
-  color_icons = true;
-  default = true;
-}
-
-devicons.set_icon {
-  ['mts'] = {
-    icon = "",
-    color = "#519aba",
-    cterm_color = "67",
-    name = "Mts",
-  }
+  color_icons = true,
+  default = true,
 }
