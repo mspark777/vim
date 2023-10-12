265630db648616de028ad2d82067e75dd527f00f
mspark777 <topangel777@gmail.com>
2023-10-13 08:31:10 +0900

    UPDATE: telescope.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -110,7 +110,7 @@ return packer.startup(function(use)
   }
   use {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
+    tag = '0.1.4',
     requires = { 'nvim-lua/plenary.nvim' }
   }
   use {
