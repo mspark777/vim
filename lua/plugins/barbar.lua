96ad39203025c248ada68ae6ffd7eb237371e9ee
mspark777 <topangel777@gmail.com>
2023-10-27 00:30:58 +0900

    ADD: barbar auto setup.

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -4,5 +4,5 @@ return {
     'lewis6991/gitsigns.nvim',
     'nvim-tree/nvim-web-devicons',
   },
-  init = function() vim.g.barbar_auto_setup = false end
+  init = function() vim.g.barbar_auto_setup = true end
 }
