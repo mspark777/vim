f8a60f507e72d8813099e59f23d8848b48021e1a
mspark777 <topangel777@gmail.com>
2023-11-08 00:42:14 +0900

    UPDATE: keymap.

--- lua/lsp/nvim-cmp.lua
+++ lua/lsp/nvim-cmp.lua
@@ -26,7 +26,7 @@ return {
         ['<C-j>'] = cmp.mapping.select_next_item(),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = false }),
       }),
