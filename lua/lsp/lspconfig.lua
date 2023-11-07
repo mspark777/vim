23b5579d92b8d73aa70f00c3d9744eaf89189c54
mspark777 <topangel777@gmail.com>
2023-11-07 11:32:18 +0900

    UPDATE: lsp.

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -1,11 +1,14 @@
 return {
   'neovim/nvim-lspconfig',
-  event = {"BufReadPre", "BufNewFile"},
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
+    "folke/neoconf.nvim",
     'hrsh7th/cmp-nvim-lsp',
     "antosha417/nvim-lsp-file-operations"
   },
   config = function()
+    require("neoconf").setup({})
+
     local lspconfig = require('lspconfig')
     local cmp_nvim_lsp = require('cmp_nvim_lsp')
     local capabilities = cmp_nvim_lsp.default_capabilities()
@@ -14,7 +17,7 @@ return {
       settings = {
         Lua = {
           dianostics = {
-            global = {"vim"}
+            global = { "vim" }
           },
           workspace = {
             library = {
@@ -42,8 +45,12 @@ return {
       capabilities = capabilities
     })
 
-    lspconfig.cssls.setup({
+    lspconfig.tailwindcss.setup({
       capabilities = capabilities
+    })
+
+    lspconfig.cssls.setup({
+      capabilities = capabilities,
     })
 
     lspconfig.html.setup({
@@ -74,12 +81,8 @@ return {
       capabilities = capabilities
     })
 
-    lspconfig.tailwindcss.setup({
-      capabilities = capabilities
-    })
-
     lspconfig.graphql.setup({
       capabilities = capabilities
     })
-   end
+  end
 }
