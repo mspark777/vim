08d44e3747811df294bc8fb74df35e735513e127
mspark777 <topangel777@gmail.com>
2024-01-17 08:35:03 +0900

    DELETE: Harpoon2.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -106,15 +106,3 @@ local function toggle_telescope(harpoon_files)
     sorter = conf.generic_sorter({}),
   }):find()
 end
-
-local harpoon = require("harpoon")
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
