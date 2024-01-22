5c15e7ae7e80f2621a0f907bd98f29352af30673
mspark777 <topangel777@gmail.com>
2024-01-22 14:36:01 +0900

    DELETE: harpoon keymap.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -88,21 +88,3 @@ local opts = { silent = true, noremap = true }
 keymap.set('n', "<A-,>", '<Cmd>BufferPrevious<CR>', opts)
 keymap.set('n', "<A-.>", '<Cmd>BufferNext<CR>', opts)
 keymap.set('n', "<A-c>", '<Cmd>BufferClose<CR>', opts)
-
--- harpoon
-local function toggle_telescope(harpoon_files)
-  local conf = require("telescope.config").values
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
-
-  require("telescope.pickers").new({}, {
-    prompt_title = "Harpoon",
-    finder = require("telescope.finders").new_table({
-      results = file_paths,
-    }),
-    previewer = conf.file_previewer({}),
-    sorter = conf.generic_sorter({}),
-  }):find()
-end
