172e61d789c7230c12524267d8acc10ed5d05644
mspark777 <topangel777@gmail.com>
2024-03-15 08:34:18 +0900

    chore: upgrade plugins

--- lua/dap/mason-nvim-dap.lua
+++ lua/dap/mason-nvim-dap.lua
@@ -16,7 +16,17 @@ return {
 			command = "node",
 			args = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/node-debug2-adapter/out/src/nodeDebug.js" },
 		}
+
 		dap.configurations.typescript = {
+			{
+				name = "Attach to process",
+				type = "node2",
+				request = "attach",
+				processId = require("dap.utils").pick_process,
+			},
+		}
+
+		dap.configurations.javascript = {
 			{
 				name = "Attach to process",
 				type = "node2",
