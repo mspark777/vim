a9b8d00e9df3cc93595ec97dc153a89a9233c8ee
mspark777 <topangel777@gmail.com>
2024-07-24 00:07:10 +0900

    refactor: update font size

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 12
+config.font_size = 14
 
 config.color_scheme = "Tokyo Night"
 config.enable_tab_bar = false
