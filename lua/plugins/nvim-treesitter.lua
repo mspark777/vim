6b27ebfae1378799518c615e4ef4b774471e27de
mspark777 <topangel777@gmail.com>
2025-07-01 17:56:31 +0900

    update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			modules = {},
+			sync_install = false,
+			ensure_installed = {},
 			ignore_install = {},
 			auto_install = true,
 			highlight = {
