13715951cac2377dd485b51487c25fc0a952e6ee
mspark777 <topangel777@gmail.com>
2025-10-23 23:14:40 +0900

    fix: colorscheme

--- init.lua
+++ init.lua
@@ -1,9 +1,8 @@
 require("mspark777.options")
 require("mspark777.lazy")
 require("mspark777.keymaps")
+vim.cmd("colorscheme tokyonight")
 
 if vim.g.mspark777_first_time then
 	require("mspark777.mason")
 end
-
-vim.cmd("colorscheme tokyonight")
