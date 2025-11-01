5310e3cec9bcc4596c986acc7e9c27e2275f230f
mspark777 <topangel777@gmail.com>
2025-11-01 16:10:37 +0900

    feat: dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -43,8 +43,29 @@ return {
 			{
 				name = "Launch",
 				type = "lldb",
-				request = "attach",
-				pid = require("dap.utils").pick_process,
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+				initCommands = function()
+					local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+					assert(
+						vim.v.shell_error == 0,
+						"failed to get rust sysroot using `rustc --print sysroot`: " .. rustc_sysroot
+					)
+
+					local script_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_lookup.py"
+					local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+					return {
+						([[!command script import '%s']]):format(script_file),
+						([[command source '%s']]):format(commands_file),
+					}
+				end,
+				-- ...,
 			},
 		}
 	end,
