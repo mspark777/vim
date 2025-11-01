e6338510304dc020a80ecb902defd77c659dcb66
mspark777 <topangel777@gmail.com>
2025-11-01 14:09:49 +0900

    feat: flash

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,5 +1,6 @@
 ---@diagnostic disable: undefined-global, duplicate-set-field, undefined-doc-name
----
+---@module "snacks"
+
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
@@ -22,16 +23,13 @@ return {
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
 		styles = {
-			notification = {
-				-- wo = { wrap = true } -- Wrap notifications
-			},
+			notification = {},
 		},
 	},
 	init = function()
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "VeryLazy",
 			callback = function()
-				-- Setup some globals for debugging (lazy-loaded)
 				_G.dd = function(...)
 					Snacks.debug.inspect(...)
 				end
@@ -39,7 +37,6 @@ return {
 					Snacks.debug.backtrace()
 				end
 
-				-- Override print to use snacks for `:=` command
 				if vim.fn.has("nvim-0.11") == 1 then
 					vim._print = function(_, ...)
 						dd(...)
@@ -48,7 +45,6 @@ return {
 					vim.print = _G.dd
 				end
 
-				-- Create some toggle mappings
 				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
 				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
 				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
